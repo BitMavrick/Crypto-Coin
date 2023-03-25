07b4f60ea03ffb7f0ed551b771b7a1f2f8975f5c
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-03-25 09:37:23 +0600

    solve link issues temporarly

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <!-- Navbar Start -->
     <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0 px-4 px-lg-5">
-        <a href="index.html" class="navbar-brand d-flex align-items-center">
+        <a href="/" class="navbar-brand d-flex align-items-center">
             <h2 class="m-0 text-primary"><img class="img-fluid me-2" src="img/icon-1.png" alt=""
                     style="width: 45px;">CryptoCoin</h2>
         </a>
@@ -10,10 +10,10 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <div class="navbar-nav ms-auto py-4 py-lg-0">
-                <router-link to="/" class="nav-item nav-link">Home</router-link>
-                <a href="#" @click.prevent="navigateTo('/about')" class="nav-item nav-link">About</a>
-                <a href="#" @click.prevent="navigateTo('/service')" class="nav-item nav-link">Service</a>
-                <a href="#" @click.prevent="navigateTo('/roadmap')" class="nav-item nav-link">Roadmap</a>
+                <a href="/" class="nav-item nav-link">Home</a>
+                <a href="/about" class="nav-item nav-link">About</a>
+                <a href="/service" class="nav-item nav-link">Service</a>
+                <a href="/roadmap" class="nav-item nav-link">Roadmap</a>
                 
                 <div class="nav-item dropdown">
                     <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
@@ -24,7 +24,7 @@
                         <a href="404.html" class="dropdown-item">404 Page</a>
                     </div>
                 </div>
-                <a href="contact.html" class="nav-item nav-link">Contact</a>
+                <a href="/contact" class="nav-item nav-link">Contact</a>
             </div>
             <div class="h-100 d-lg-inline-flex align-items-center d-none">
                 <a class="btn btn-square rounded-circle bg-light text-primary me-2" href=""><i
@@ -42,11 +42,6 @@
 <script>
 export default {
     name: 'Navbar',
-    methods: {
-    navigateTo(route) {
-      this.$router.push(route);
-    }
-  },
 }
 
 </script>
